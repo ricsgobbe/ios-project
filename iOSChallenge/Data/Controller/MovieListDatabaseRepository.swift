7fc72d12f3aaa14a5f00a88248ddfed03b34d2bb
Ricardo Sgobbe <sgobbericardo@gmail.com>
2018-10-07 14:41:42 -0300

    realm database added.

---
+++ iOSChallenge/Data/Controller/MovieListDatabaseRepository.swift
@@ -0,0 +1,9 @@
+//
+//  MovieListDatabaseRepository.swift
+//  iOSChallenge
+//
+//  Created by Mac on 07/10/18.
+//  Copyright © 2018 Ricardo Sgobbe. All rights reserved.
+//
+
+import Foundation
---
+++ iOSChallenge/Data/Data Source/Database/MovieDatabaseDataSource.swift
@@ -0,0 +1,9 @@
+//
+//  MovieDatabaseDataSource.swift
+//  iOSChallenge
+//
+//  Created by Mac on 07/10/18.
+//  Copyright © 2018 Ricardo Sgobbe. All rights reserved.
+//
+
+import Foundation
